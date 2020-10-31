2b350db1077c4a267b442ad07c564e614de491c3
Corey Albright <Hawaiideveloper@users.noreply.github.com>
2020-10-31 11:03:31 -0600

    Update macOSX_payloader.sh

--- macOSX_payloader.sh
+++ macOSX_payloader.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-macOSX_Installer="https://raw.github.com/Hawaiideveloper/MERN_nodeJS_setup/macOSX_installer"
-
-https://raw.github.com/Hawaiideveloper/MERN_nodeJS_setup/macOSX_installer/automated_preinstall.sh
+curl -fsSL https://raw.github.com/Hawaiideveloper/MERN_nodeJS_setup/macOSX_installer/automated_preinstall.sh | /bin/bash
 
 
 #curl -fsSL https://raw.github.com/Hawaiideveloper/MERN_nodeJS_setup/macOSX_installer/macOSX_payloader.sh
 
 
 #Downloads the file so the user can examine later
-curl -LJO $macOSX_Installer/automated_preinstall.sh
-curl -LJO $macOSX_Installer/post_installer.sh
-curl -LJO $macOSX_Installer/database_installer-mac_osx.sh
+#curl -LJO $macOSX_Installer/automated_preinstall.sh
+#curl -LJO $macOSX_Installer/post_installer.sh
+#curl -LJO $macOSX_Installer/database_installer-mac_osx.sh
 
 
 
 
 #Downloads and installs the scripts in order
-curl -fsSL $macOSX_Installer/automated_preinstall.sh 
-curl -fsSL $macOSX_Installer/post_installer.sh 
-curl -fsSL $macOSX_Installer/database_installer-mac_osx.sh
+#curl -fsSL $macOSX_Installer/automated_preinstall.sh 
+#curl -fsSL $macOSX_Installer/post_installer.sh 
+#curl -fsSL $macOSX_Installer/database_installer-mac_osx.sh
 
