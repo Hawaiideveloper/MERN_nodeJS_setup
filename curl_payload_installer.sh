ee948b71b6894db660f487850867245f92ff9fe7
Corey Albright <Hawaiideveloper@users.noreply.github.com>
2020-10-31 09:58:20 -0600

    Update curl_payload_installer.sh

--- curl_payload_installer.sh
+++ curl_payload_installer.sh
@@ -1,1 +1,4 @@
 curl -fsSL https://raw.github.com/[user_name]/[github_repo_project]/[branch]/path/install.sh
+
+
+curl -fsSL https://github.com/Hawaiideveloper/MERN_nodeJS_setup/docker_step/curl_payload_installer.sh
