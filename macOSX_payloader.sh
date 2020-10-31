87992b753055cc71302742817dd5ef7292e720b1
Corey Albright <Hawaiideveloper@users.noreply.github.com>
2020-10-31 10:57:12 -0600

    Update macOSX_payloader.sh

--- macOSX_payloader.sh
+++ macOSX_payloader.sh
@@ -2,7 +2,7 @@
 
 macOSX_Installer="https://raw.github.com/Hawaiideveloper/MERN_nodeJS_setup/macOSX_installer"
 
-
+https://raw.github.com/Hawaiideveloper/MERN_nodeJS_setup/macOSX_installer/automated_preinstall.sh
 
 
 #curl -fsSL https://raw.github.com/Hawaiideveloper/MERN_nodeJS_setup/macOSX_installer/macOSX_payloader.sh
@@ -17,7 +17,7 @@ curl -LJO $macOSX_Installer/database_installer-mac_osx.sh
 
 
 #Downloads and installs the scripts in order
-curl -fsSL $macOSX_Installer/automated_preinstall.sh | /bin/bash
-curl -fsSL $macOSX_Installer/post_installer.sh | /bin/bash
+curl -fsSL $macOSX_Installer/automated_preinstall.sh 
+curl -fsSL $macOSX_Installer/post_installer.sh 
 curl -fsSL $macOSX_Installer/database_installer-mac_osx.sh
 
